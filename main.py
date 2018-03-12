d1a3b1945f4595dd91887dae44ff9e466012c3ab
AaronEnnis <aaronennis96@outlook.com>
2018-03-12 15:52:33 +0000

    better transcribing

--- main.py
+++ main.py
@@ -241,42 +241,52 @@ def tuner(data):
 
 
 def func():            
-    NOTE_MIN = 21     
-    NOTE_MAX = 108      
+    NOTE_MIN = 40       # E2
+    NOTE_MAX = 76       # E4
     FSAMP = 22050       # Sampling frequency in Hz
-    FRAME_SIZE = 2048   # How many samples per frame?
-    FRAMES_PER_FFT = 16 # FFT takes average across how many frames?
+    FRAME_SIZE = 2048   # samples per frame
+    FRAMES_PER_FFT = 16  # FFT takes average across how many frames?
     
     ######################################################################
     # Derived quantities from constants above. Note that as
-    # SAMPLES_PER_FFT goes up, the frequency step size decreases (so
+    # SAMPLES_PER_FFT goes up, the frequency step size decreases (sof
     # resolution increases); however, it will incur more delay to process
     # new sounds.
     
-    SAMPLES_PER_FFT = FRAME_SIZE*FRAMES_PER_FFT
-    FREQ_STEP = float(FSAMP)/SAMPLES_PER_FFT
+    SAMPLES_PER_FFT = FRAME_SIZE * FRAMES_PER_FFT
+    FREQ_STEP = float(FSAMP) / SAMPLES_PER_FFT
     
     ######################################################################
     # For printing out notes
     
-    NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
+    NOTE_NAMES = 'E F F# G G# A A# B C C# D D#'.split()
+    
     
     ######################################################################
     # These three functions are based upon this very useful webpage:
     # https://newt.phys.unsw.edu.au/jw/notes.html
     
-    def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
-    def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
-    def note_name(n): return NOTE_NAMES[n % 12] + str(np.floor(n/12 - 1))
+    def freq_to_number(f): return 76 + 12 * np.log2(f / 659.255)
+    
+    
+    def number_to_freq(n): return 659.255 * 2.0**((n - 76) / 12.0)
+    
+    
+    def note_name(n):
+        return NOTE_NAMES[n % NOTE_MIN % len(NOTE_NAMES)] + str(int(n / 12 - 1))
     
     ######################################################################
     # Ok, ready to go now.
     
     # Get min/max index within FFT of notes we care about.
     # See docs for numpy.rfftfreq()
-    def note_to_fftbin(n): return number_to_freq(n)/FREQ_STEP
-    imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
-    imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
+    
+    
+    def note_to_fftbin(n): return number_to_freq(n) / FREQ_STEP
+    
+    
+    imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
+    imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
     
     # Allocate space to run an FFT. 
     buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
@@ -342,15 +352,14 @@ def func():
     
         # Get frequency of maximum response in range
         freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
-    
+
         # Get note number and nearest note
         n = freq_to_number(freq)
         n0 = int(round(n))
     
         # Console output once we have a full buffer
         num_frames += 1
-    
-        #if num_frames >= FRAMES_PER_FFT:
+
         print ('freq: ', freq, '  ', note_name(n0))
         notes.append(note_name(n0))
     
@@ -362,14 +371,22 @@ def func():
 
     path = os.path.join('C:/Users/aaron/Desktop/Music-Application','recording.wav')
     record_to_file(path,r,sample_width)
-    print(notes)
+
     #roughly the note being played every half a second    
-    c = 0  
+    num_of_notes = len(notes) 
+    notes_per_sec = num_of_notes / elapsed
+    note_per_hsec = notes_per_sec / 2
+    c = 1
+    p = 1
     for i in notes:
-        if c == 7:
+        if c == np.floor(note_per_hsec):
             print(i)
             c = 0
+            p += 1
         c += 1
+        
+    print(p)
+        
 
 
 
